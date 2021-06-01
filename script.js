8e2dd16d8708c7e46fb798e55534e36f3b354708
Anna Louise Oliveira <anna.louise@hotmail.com>
2021-06-01 19:49:46 -0300

    Correção das tags de classe e id

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Local de destino 
 const pilhaInicial = document.getElementById("inicial");
- const pilhaMeio = document.getElementById("meio");
- const pilhaFinal = document.getElementById("final");
- const main = document.getElementById('divPilhas')
+const pilhaMeio = document.getElementById("meio");
+const pilhaFinal = document.getElementById("final");
+const main = document.getElementById('divPilhas')
 
 // Torres
 const torreInicial = document.createElement('div');
@@ -17,33 +17,27 @@ pilhaFinal.appendChild(torreFinal);
 
 
 //Pilhas
-
-const inicio = document.createElement('div')
-inicio.classList.add('pilha','inicial')
-main.appendChild(inicio)
-
-const meio = document.createElement('div')
-meio.classList.add('pilha','meio')
-main.appendChild(meio)
-
-const final = document.createElement('div')
-final.classList.add('pilha','final')
-main.appendChild(final)
+// const inicio = document.createElement('div')
+// inicio.classList.add('pilha','inicial')
+// main.appendChild(inicio)
+// const meio = document.createElement('div')
+// meio.classList.add('pilha','meio')
+// main.appendChild(meio)
+// const final = document.createElement('div')
+// final.classList.add('pilha','final')
+// main.appendChild(final)
 
 //Discos
 
 const disco1 = document.createElement('div')
 disco1.classList.add('disco','um')
 pilhaInicial.appendChild(disco1)
-
 const disco2 = document.createElement('div')
 disco2.classList.add('disco','dois')
 pilhaInicial.appendChild(disco2)
-
 const disco3 = document.createElement('div')
 disco3.classList.add('disco','tres')
 pilhaInicial.appendChild(disco3)
-
 const disco4 = document.createElement('div')
 disco4.classList.add('disco','quatro')
 pilhaInicial.appendChild(disco4)
